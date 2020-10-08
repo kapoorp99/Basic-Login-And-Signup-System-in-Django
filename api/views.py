718501b6915333099801d538abe43c0df0015366
kapoorp99 <kapoorprakhar99@gmail.com>
2020-10-08 18:52:02 +0530

    Added rest api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AuthorSerializer
+from .models import Author
 
-# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().order_by('name')
+    serializer_class = AuthorSerializer
--- eblogs/settings.py
+++ eblogs/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
